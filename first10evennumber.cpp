53bdd9b0c95106a4acffe3393aed8ee3e5e0356d
Aayush Basnet <122598100+Aayush-Basnet@users.noreply.github.com>
2023-01-29 19:42:48 +0545

    First 10 Even Number

---
+++ first10evennumber.cpp
@@ -0,0 +1,19 @@
+//WAP to display the first 10 even number
+#include<iostream>
+using namespace std;
+int main(){
+int i=0,n;
+cout<<"Enter the number till: "<<n<<endl;
+cin>>n;
+if(i<=n){
+do{
+
+    cout<<""<<i<<endl;
+    i++;
+
+}while(i<=20);
+}
+
+
+return 0;
+}
