7222d69f2e9067500a3edafc12a7de9fc3be2292
Aayush Basnet <122598100+Aayush-Basnet@users.noreply.github.com>
2023-02-13 18:44:59 +0545

    Default Constructor

---
+++ ClassDefaultConstructor.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+class Employee{
+	public:
+		Employee(){
+			cout<<"Default Constructor Invoked"<<endl;
+		}
+};
+int main(){
+	Employee e1;
+	Employee e2;
+	Employee e3;
+return 0;	
+}
