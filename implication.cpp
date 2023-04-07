f068b65d608718243951ab676fdedb470162e9ba
Aayush Basnet <122598100+Aayush-Basnet@users.noreply.github.com>
2023-04-07 19:52:14 +0545

    Implication
    Write a program to display the truth table of Implication

---
+++ implication.cpp
@@ -0,0 +1,15 @@
+//Write a program to display the truth table of Implication
+
+#include<iostream>
+using namespace std;
+int main(){
+	
+	cout<<"A\tB\t A->B"<<endl;
+	cout<<"--------------------"<<endl;
+	cout<<"0\t0\t "<<(!0||0)<<endl;
+	cout<<"0\t1\t "<<(!0||1)<<endl;
+	cout<<"1\t0\t "<<(!1||0)<<endl;
+	cout<<"1\t1\t "<<(!1||1)<<endl;
+	
+	return 0;
+}
